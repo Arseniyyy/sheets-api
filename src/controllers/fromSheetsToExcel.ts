44a424744707b1cae9ed1ecf50fbaaaaa493679a
Arseniy Koshelnik <arskosh05@mail.ru>
2020-10-31 14:10:30 +0700

    Deleted readExcelFile.ts and added fromSheetsToExcel.ts

--- src/controllers/fromSheetsToExcel.ts
+++ src/controllers/fromSheetsToExcel.ts
@@ -1,16 +1,15 @@
 import { oauthClient } from '../google/sheets'
 import { getAccessToSheets } from '../google/sheets'
-import ExcelJS, { Workbook, Worksheet } from 'exceljs'
 import { Request, Response } from 'express'
 import { sheets_v4 } from 'googleapis'
-// import fs from 'fs'
+import xlsx from 'xlsx'
 
 export async function fromSheetsToExcel(req: Request, res: Response) {
   try {
     const sheets: sheets_v4.Sheets = getAccessToSheets(oauthClient)
 
-    const id = req.query.id
-    const range = req.query.range
+    // const id = req.query.id
+    // const range = req.query.range
 
     // const { data } = await sheets.spreadsheets.values.get({
     //   spreadsheetId: id as string,
@@ -18,7 +17,14 @@ export async function fromSheetsToExcel(req: Request, res: Response) {
     //   auth: oauthClient
     // })
 
-    const workbook: Workbook = new ExcelJS.Workbook()
+    const workbook: xlsx.WorkBook = xlsx.readFile('E:/Programming/google_apis/sheets/src/excel/files/Financial_Sample.xlsx', {})
+    const worksheet: xlsx.WorkSheet = workbook.Sheets['Sheet1']
+
+    console.log(worksheet)
+
+    return res.json({
+      message: 'file read'
+    })
   } catch (error) {
     return console.error(error)
   }
